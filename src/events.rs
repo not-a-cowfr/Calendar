f554760eea289004955adcb1e21d4649377885f8
not a cow <104355555+not-a-cowfr@users.noreply.github.com>
2025-07-29 16:10:25 -0700

    formatting

--- src/events.rs
+++ src/events.rs
@@ -1,50 +1,53 @@
 use std::collections::HashMap;
 
-use ratatui::{
-    style::{Color, Modifier, Style},
-    widgets::calendar::CalendarEventStore,
-};
+use ratatui::style::{Color, Modifier, Style};
+use ratatui::widgets::calendar::CalendarEventStore;
 use time::Date;
 
 #[allow(non_snake_case)]
 pub struct Events {
-    pub data: HashMap<Date, (String, Style)>,
-    pub SELECTED_DATE: Date,
+	pub data:          HashMap<Date, (String, Style)>,
+	pub SELECTED_DATE: Date,
 }
 
 impl Events {
-    pub const SELECTED_STYLE: Style = Style::new()
-        .fg(Color::White)
-        .bg(Color::Red)
-        .add_modifier(Modifier::BOLD);
+	pub const SELECTED_STYLE: Style = Style::new()
+		.fg(Color::White)
+		.bg(Color::Red)
+		.add_modifier(Modifier::BOLD);
 
-    pub fn new(selected_date: Date) -> Self {
-        Events {
-            data: HashMap::new(),
-            SELECTED_DATE: selected_date,
-        }
-    }
+	pub fn new(selected_date: Date) -> Self {
+		Events {
+			data:          HashMap::new(),
+			SELECTED_DATE: selected_date,
+		}
+	}
 
-    pub fn add_selected(mut self) -> Self {
-        self.data
-            .insert(self.SELECTED_DATE, ("".to_string(), Self::SELECTED_STYLE));
-        self
-    }
+	pub fn add_selected(mut self) -> Self {
+		self.data
+			.insert(self.SELECTED_DATE, ("".to_string(), Self::SELECTED_STYLE));
+		self
+	}
 
-    pub fn get_store(&self) -> CalendarEventStore {
-        let mut list = CalendarEventStore::today(
-            Style::default()
-                .add_modifier(Modifier::BOLD)
-                .bg(Color::Blue),
-        );
-        for (date, (_name, style)) in &self.data {
-            list.add(*date, *style);
-        }
-        list
-    }
+	pub fn get_store(&self) -> CalendarEventStore {
+		let mut list = CalendarEventStore::today(
+			Style::default()
+				.add_modifier(Modifier::BOLD)
+				.bg(Color::Blue),
+		);
+		for (date, (_name, style)) in &self.data {
+			list.add(*date, *style);
+		}
+		list
+	}
 
-    pub fn add(mut self, date: Date, name: String, style: Style) -> Self {
-        self.data.insert(date, (name, style));
-        self
-    }
+	pub fn add(
+		mut self,
+		date: Date,
+		name: String,
+		style: Style,
+	) -> Self {
+		self.data.insert(date, (name, style));
+		self
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -13,68 +13,71 @@ use time::{Date, Month, OffsetDateTime};
 use crate::events::Events;
 
 fn main() -> Result<()> {
-    color_eyre::install()?;
-    let mut terminal = ratatui::init();
+	color_eyre::install()?;
+	let mut terminal = ratatui::init();
 
-    let mut selected_date = OffsetDateTime::now_local()?.date();
+	let mut selected_date = OffsetDateTime::now_local()?.date();
 
-    terminal.clear()?;
-    loop {
-        terminal.draw(|frame| render(frame, selected_date))?;
-        if let Some(key) = event::read()?.as_key_press_event() {
-            match key.code {
-                KeyCode::Char('q') => break Ok(()),
-                KeyCode::Tab => selected_date = next_month(selected_date),
-                KeyCode::BackTab => selected_date = prev_month(selected_date),
-                KeyCode::Left => selected_date -= 1.days(),
-                KeyCode::Down => selected_date += 1.weeks(),
-                KeyCode::Up => selected_date -= 1.weeks(),
-                KeyCode::Right => selected_date += 1.days(),
-                _ => {}
-            }
-        }
-    }
+	terminal.clear()?;
+	loop {
+		terminal.draw(|frame| render(frame, selected_date))?;
+		if let Some(key) = event::read()?.as_key_press_event() {
+			match key.code {
+				| KeyCode::Char('q') => break Ok(()),
+				| KeyCode::Tab => selected_date = next_month(selected_date),
+				| KeyCode::BackTab => selected_date = prev_month(selected_date),
+				| KeyCode::Left => selected_date -= 1.days(),
+				| KeyCode::Down => selected_date += 1.weeks(),
+				| KeyCode::Up => selected_date -= 1.weeks(),
+				| KeyCode::Right => selected_date += 1.days(),
+				| _ => {},
+			}
+		}
+	}
 }
 
 fn next_month(date: Date) -> Date {
-    if date.month() == Month::December {
-        date.replace_month(Month::January)
-            .unwrap()
-            .replace_year(date.year() + 1)
-            .unwrap()
-    } else {
-        date.replace_month(date.month().next()).unwrap()
-    }
+	if date.month() == Month::December {
+		date.replace_month(Month::January)
+			.unwrap()
+			.replace_year(date.year() + 1)
+			.unwrap()
+	} else {
+		date.replace_month(date.month().next()).unwrap()
+	}
 }
 
 fn prev_month(date: Date) -> Date {
-    if date.month() == Month::January {
-        date.replace_month(Month::December)
-            .unwrap()
-            .replace_year(date.year() - 1)
-            .unwrap()
-    } else {
-        date.replace_month(date.month().previous()).unwrap()
-    }
+	if date.month() == Month::January {
+		date.replace_month(Month::December)
+			.unwrap()
+			.replace_year(date.year() - 1)
+			.unwrap()
+	} else {
+		date.replace_month(date.month().previous()).unwrap()
+	}
 }
 
-fn render(frame: &mut Frame, date: Date) {
-    let area = frame.area();
+fn render(
+	frame: &mut Frame,
+	date: Date,
+) {
+	let area = frame.area();
 
-    let header = Text::from_iter([
-        Line::from("Calendar".bold()),
-        Line::from("<Q> Quit| <tab> Next Month | <shift + tab> Previous Month, <arrow keys> Move"),
-        Line::from(format!("Current date: {}", date)),
-    ]);
+	let header = Text::from_iter([
+		Line::from("Calendar".bold()),
+		Line::from("<Q> Quit| <tab> Next Month | <shift + tab> Previous Month, <arrow keys> Move"),
+		Line::from(format!("Current date: {}", date)),
+	]);
 
-    frame.render_widget(header.centered(), area);
+	frame.render_widget(header.centered(), area);
 
-    let events = Events::new(date).add_selected();
+	let events = Events::new(date).add_selected();
 
-    let calendar = Monthly::new(date, events.get_store())
-        .default_style(Style::new().bold().bg(Color::Rgb(50, 50, 50)))
-        .show_weekdays_header(Style::default())
-        .show_surrounding(Style::new().dim());
+	let calendar = Monthly::new(date, events.get_store())
+		.default_style(Style::new().bold().bg(Color::Rgb(50, 50, 50)))
+		.show_weekdays_header(Style::default())
+		.show_surrounding(Style::new().dim());
 
-    frame.render_widget(calendar, area);
+	frame.render_widget(calendar, area);
 }
